6240cd90a41104c2dd1239e1f20a329e03746862
sebastian <sebasjimenez1121@gmail.com>
2023-08-17 04:16:12 -0500

    se modifico el filtro

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,14 @@
 const openModal = document.querySelectorAll('.tarjeta');
 const closeModal = document.querySelectorAll('.close-modal');
+const openNewModal = document.querySelectorAll('#open');
+
+openNewModal.forEach(btn => {
+    btn.addEventListener("click", ()=>{
+        const modalId = btn.getAttribute("data-modal");
+        const modal = document.getElementById(modalId);
+        modal.classList.add('show-modal');
+    });
+});
 
 openModal.forEach(btn => {
     btn.addEventListener("click", () => {
